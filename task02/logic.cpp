a095a64ef055c2b5694f7a9c922b45545a3a89e4
polin-sv <polinasvetlay@icloud.com>
2026-03-16 17:33:44 +0300

    update task02

--- task02/logic.cpp
+++ task02/logic.cpp
@@ -1,17 +1,34 @@
 #include "logic.h"
 
+//bool is_perfect_number(int number) {
+//	if (number % 2 == 1) {
+//		return false;
+//	}
+//
+//	int sum = 1; int n = number / 2;
+//	for (int div = 2; div <= n; div++)
+//	{
+//		if (number % div == 0) {
+//			sum += div;
+//		}
+//	}
+//
+//	return sum == number;
+//}
+
 bool is_perfect_number(int number) {
 	if (number % 2 == 1) {
 		return false;
 	}
+	int size = 5;
+	int perfect_array[]{ 6,28,8128,33550336,858986056 };
 
-	int sum = 1; int n = number / 2;
-	for (int div = 2; div <= n; div++)
+	for (int i = 0; i < size; i++)
 	{
-		if (number % div == 0) {
-			sum += div;
+		if (number == perfect_array[i]) {
+			return true;
 		}
 	}
 
-	return sum == number;
+	return false;
 }
